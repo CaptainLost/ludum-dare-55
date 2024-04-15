461acf72d179d1b31d20a2cd78dca2563872e9cf
CaptainLost <biggun632@gmail.com>
2024-04-15 11:25:04 +0200

    Level Finishing

--- Assets/Scripts/Gameplay/Level/LevelFinisher.cs
+++ Assets/Scripts/Gameplay/Level/LevelFinisher.cs
@@ -1,11 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LevelFinisher : MonoBehaviour
 {
     [SerializeField]
     private List<SignalReceiverObject> m_gameFinisherSignals;
 
+    public UnityEvent OnLevelFinished;
 
+    private int m_activeSignals = 0;
+
+    private void OnEnable()
+    {
+        RegisterSignalEvents();
+    }
+
+    private void OnDisable()
+    {
+        UnregisterSignalEvents();
+    }
+
+    private void RegisterSignalEvents()
+    {
+        foreach (SignalReceiverObject receiver in m_gameFinisherSignals)
+        {
+            receiver.OnSignalChanged += OnAnySignalUpdated;
+        }
+    }
+
+    private void UnregisterSignalEvents()
+    {
+        foreach (SignalReceiverObject receiver in m_gameFinisherSignals)
+        {
+            receiver.OnSignalChanged -= OnAnySignalUpdated;
+        }
+    }
+
+    private void OnAnySignalUpdated(bool active)
+    {
+        if (active)
+        {
+            m_activeSignals++;
+        }
+        else
+        {
+            m_activeSignals--;
+        }
+
+        if (m_activeSignals >= m_gameFinisherSignals.Count)
+        {
+            OnLevelFinished?.Invoke();
+            Debug.Log("Level finished");
+        }
+    }
 }
